f6767485cae63ac02602e62ef5fa1eaaa4ff423a
Loudcow67 <logan.soulliere@ontariotechu.net>
2020-10-23 17:04:07 -0400

    Activity

---
+++ INFR3110-F2020-Lesson6-ICE-main/Assets/_Scripts/TileManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class TileManager : MonoBehaviour
+{
+    private Queue<GameObject> tilePool;
+
+    public int maxTiles;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tilePool = new Queue<GameObject>();
+        buildTilePool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void buildTilePool()
+    {
+        for (int count = 0; count < maxTiles; count++)
+        {
+            var tempTile = TileFactory.Instance().CreateTile();
+            tempTile.SetActive(false);
+            tilePool.Enqueue(tempTile);
+        }
+    }
+
+    public GameObject GetTile()
+    {
+        var tempTile = tilePool.Dequeue();
+        tempTile.SetActive(true);
+        return tempTile;
+    }
+
+    public void returnTile(GameObject tile)
+    {
+        tile.SetActive(false);
+        tilePool.Enqueue(tile);
+    }
+}
